09138ccf3715eef851538e81ae97780723ad283c
kevin770113 <kevin770113@gmail.com>
2026-07-28 14:38:28 +0800

    Update database.ts

--- src/types/database.ts
+++ src/types/database.ts
@@ -1,8 +1,15 @@
+// 定義帶有屬性的招式結構
+export interface PokemonMove {
+  name: string;
+  type: string;
+}
+
+// 更新寶可夢資料庫介面
 export interface PokemonData {
   dex: number;
   speciesId: string;
   speciesName: string;
   types: string[]; 
-  fastMoves: string[];
-  chargedMoves: string[];
+  fastMoves: PokemonMove[];
+  chargedMoves: PokemonMove[];
 }
