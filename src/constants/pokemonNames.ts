2b474d921d1548b81cfc174a504d20ca30f7f6cd
kevin770113 <kevin770113@gmail.com>
2026-07-29 13:27:42 +0800

    Create pokemonNames.ts

---
+++ src/constants/pokemonNames.ts
@@ -0,0 +1,52 @@
+export const POKEMON_NAMES: Record<string, string> = {
+  // 關都地區 (Gen 1)
+  "venusaur": "妙蛙花",
+  "venusaur_mega": "超級妙蛙花",
+  "charizard": "噴火龍",
+  "charizard_mega_x": "超級噴火龍 X",
+  "charizard_mega_y": "超級噴火龍 Y",
+  "blastoise": "水箭龜",
+  "blastoise_mega": "超級水箭龜",
+  "pikachu": "皮卡丘",
+  "machamp": "怪力",
+  "machamp_shadow": "怪力 (暗影)",
+  "gengar": "耿鬼",
+  "snorlax": "卡比獸",
+  "dragonite": "快龍",
+  "mewtwo": "超夢",
+  "mewtwo_armored": "裝甲超夢",
+  
+  // 城都與豐緣地區 (Gen 2 - Gen 3)
+  "tyranitar": "班基拉斯",
+  "ho_oh": "鳳王",
+  "lugia": "洛奇亞",
+  "swampert": "巨沼怪",
+  "gardevoir": "沙奈朵",
+  "metagross": "巨金怪",
+  "rayquaza": "烈空坐",
+  "kyogre": "蓋歐卡",
+  "groudon": "固拉多",
+  "salamence": "暴飛龍",
+
+  // 神奧與合眾地區 (Gen 4 - Gen 5)
+  "garchomp": "烈咬陸鯊",
+  "lucario": "路卡利歐",
+  "togekiss": "波克基斯",
+  "dialga": "帝牙盧卡",
+  "palkia": "帕路奇亞",
+  "giratina_altered": "騎拉帝納 (別種)",
+  "giratina_origin": "騎拉帝納 (起源)",
+  "cresselia": "克雷色利亞",
+  "excadrill": "龍頭地鼠",
+  "stunfisk_galarian": "伽勒爾泥巴魚",
+
+  // 常見地區型態與其他 Meta
+  "ninetales_alolan": "阿羅拉九尾",
+  "marowak_alolan": "阿羅拉嘎啦嘎啦",
+  "obstagoon": "堵攔熊",
+  "trevenant": "朽木妖",
+  "walrein": "帝牙海獅",
+  "azumarill": "瑪力露麗",
+  "medicham": "恰雷姆",
+  "registeel": "雷吉斯奇魯"
+};
